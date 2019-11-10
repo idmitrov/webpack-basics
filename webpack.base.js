eaf21563a2ebf95a90c7f85575e6848d0effbedc
Dimitar <mailto.dimitrov@gmail.com>
2019-11-10 16:10:58 +0200

    Add Babel 7

--- webpack.base.js
+++ webpack.base.js
@@ -3,6 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
